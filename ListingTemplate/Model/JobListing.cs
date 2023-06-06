6ae69e076797b857924fe7e40b9a73c25fa9b815
Malchyk Nastya <nastyamalchyk.akapelmen@gmail.com>
2023-06-06 21:19:02 +0300

    created job-listing model

---
+++ ListingTemplate/Model/JobListing.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ListingTemplate.Model
+{
+    public class JobListing
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public DateTime Created { get; set; }
+
+    }
+}
