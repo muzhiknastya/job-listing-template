1fe09937345bc48d91791f6f31d375fedad2fc4c
Malchyk Nastya <nastyamalchyk.akapelmen@gmail.com>
2023-06-11 19:11:49 +0300

    changed local database to the database hosted on azure

---
+++ ListingTemplate/obj/Release/net6.0/win-x86/PubTmp/EFSQLScripts/ListingTemplate.Data.ApplicationDbContext.sql
@@ -0,0 +1,36 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230611152604_AddJobListingToDb')
+BEGIN
+    CREATE TABLE [JobListing] (
+        [Id] int NOT NULL IDENTITY,
+        [Title] nvarchar(max) NOT NULL,
+        [Description] nvarchar(max) NOT NULL,
+        [Author] nvarchar(max) NOT NULL,
+        [Created] datetime2 NOT NULL,
+        CONSTRAINT [PK_JobListing] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230611152604_AddJobListingToDb')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20230611152604_AddJobListingToDb', N'6.0.16');
+END;
+GO
+
+COMMIT;
+GO
+
